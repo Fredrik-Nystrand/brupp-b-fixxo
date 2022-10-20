8a3933f2a01fcefec7613f848403157751c6edf4
Anna <annaelg@live.se>
2022-10-20 21:05:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Footer from './components/Footer/Footer'
+import Support from './components/Support/Support';
 
 function App() {
   return (
     <div className="container-outer">
-      <div className="container-inner"></div>
+      <div className="container-inner">
+      <Support/>
+      </div>
     </div>
   );
 }
