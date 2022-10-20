bbdaad48e1a79fd3e4b17dade06df94ef0c799b0
Fredrik Lundkvist <fredrik.1994.lundkvist@gmail.com>
2022-10-20 13:24:23 +0200

    Navbar påbörjad

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="container-outer">
+      <Navbar /> 
       <div className="container-inner"></div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,18 @@ import styles from './Navbar.module.css'
 const Navbar = () => {
   return ( 
     <div className={`${styles.navbar}`}>
-      Navbar
+      <div className={`${styles.dflex}`}>
+          <div className={`${styles.navlinks_left}`}>
+        <h2 className={`${styles.logo}`}>Fixxo.</h2>
+            <li className={`${styles.links}`}>Home</li>
+            <li className={`${styles.links}`}>Categories</li>
+            <li className={`${styles.links}`}>Products</li>
+        </div>
+        <div className={`${styles.navlinks_right}`}>
+          <li className={`${styles.links}`}>Login</li>
+          <li className={`${styles.links}`}>My Cart</li>
+        </div>
+      </div>
     </div>
   )
 }
