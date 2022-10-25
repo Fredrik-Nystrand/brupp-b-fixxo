6291dee35100fd40d053169e1f6cad356204d374
Anna <annaelg@live.se>
2022-10-25 10:19:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
+    <>
+      <Hero/>
     <div className="container-outer">
       <div className="container-inner"></div>
     </div>
+
+    </>
   );
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import hero from '../../assets/Hero/Hero.svg'
+
+const Hero = () => {
+  return (
+  <header className={`${styles.hero_bg}`}>
+     <div className={`${styles.container}`}>
+      <div className={`${styles.hero}`}>  
+
+            <div className={`${styles.text_group}`}>
+              <h3>Get up to 40% off</h3>
+              <h1>Don't Miss This Opportunity</h1>
+              <p>Online shopping free home delivery over $100</p>
+              <button className={`${styles.btn} ${styles.btn_hero}`}>Shop now</button>
+            </div>
+
+            <div className={`${styles.img}`}>
+              <img src={hero} alt="girl holding a hat" />
+            </div>
+
+        </div>
+      </div>
+
+    </header>
+  )
+}
+
+export default Hero
