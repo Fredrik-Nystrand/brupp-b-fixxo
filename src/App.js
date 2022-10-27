3d7c8778d6cb0bcc15b1c50d195ebe269d0c8ba7
Fredrik Nystrand <f.nystrand@gmail.com>
2022-10-27 10:15:53 +0200

    added useBreakpoint, fake products and global css styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Footer from './components/Footer/Footer'
+import "./App.css"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <div className="container-outer">
       <div className="container-inner"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
