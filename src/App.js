d7636d33a783bddccc8bf1a84adef3c7a0f4d6e4
Fredrik Lundkvist <fredrik.1994.lundkvist@gmail.com>
2022-10-27 12:53:39 +0200

    uppdaterade navbaren -Fredrik L

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-import './App.css';
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar';
-=======
 import "./App.css"
 import Footer from "./components/Footer/Footer"
->>>>>>> 3d7c8778d6cb0bcc15b1c50d195ebe269d0c8ba7
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <div className="container-outer">
       <Navbar /> 
+      <Footer />
       <div className="container-inner"></div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import { FaSearch } from 'react-icons/fa';
+import { BiSearch } from 'react-icons/bi';
+import { BiShoppingBag } from 'react-icons/bi'
 
 const Navbar = () => {
   return ( 
@@ -11,14 +12,17 @@ const Navbar = () => {
               <li className={`${styles.links}`}>Home</li>
               <li className={`${styles.links}`}>Categories</li>
               <li className={`${styles.links}`}>Products</li>
+              <li className={`${styles.links}`}>Pages</li>
+              <li className={`${styles.links}`}>Blog</li>
         </div>
         <div className={`${styles.navlinks_right}`}>
           <li className={`${styles.links}`}>Login</li>
-          <li className={`${styles.links}`}>My Cart</li>
-          <div className={`${styles.links}`}>
-            <button className={`${styles.btn_search}`}><FaSearch className={`${styles.fa_search}`}/></button>
+          <li className={`${styles.links}`}>
+            <button className={`${styles.btn_bag}`}><BiShoppingBag className={`${styles.bi_bag}`}/></button>
+            <button className={`${styles.btn_search}`}><BiSearch className={`${styles.bi_search}`}/></button>
             <input type="text" className={`${styles.input_search}`} placeholder="Type to Search..."></input>
-          </div>
+          </li>
+
 
         </div>
       </div>
