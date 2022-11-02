7acf2470de190b2100526c76d3082fda51d5138e
Anna <annaelg@live.se>
2022-11-02 14:01:40 +0100

    små fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './Hero.module.css'
 import hero from '../../assets/Hero/Hero.svg'
+import ButtonMain from '../ButtonMain/ButtonMain'
+import useBreakpoint from '../../shared/hooks/useBreakpoint'
 
 const Hero = () => {
+  const br = useBreakpoint();
+
   return (
   <header className={`${styles.hero_bg}`}>
       <div className={`${styles.hero}`}>  
@@ -11,11 +15,15 @@ const Hero = () => {
               <h3>Get up to 40% off</h3>
               <h1>Don't Miss This Opportunity</h1>
               <p>Online shopping free home delivery over $100</p>
-              <button className={`${styles.btn} ${styles.btn_hero}`}>Shop now</button>
+              <ButtonMain
+               text="Shop Now"
+               align = "left"
+               onClick={() => console.log("test")}
+              />
             </div>
 
             <div className={`${styles.img}`}>
-              <img src={hero} alt="girl holding a hat" />
+              <img src={hero} alt="girl holding a hat" className={br.lessThan("md") ? 'display_none' : 'display_show'} />
             </div>
 
         </div>
