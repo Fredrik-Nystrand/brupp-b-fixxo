5382e597839e08158635a97594aee792b97e0ff0
Fredrik Nystrand <f.nystrand@gmail.com>
2022-11-03 15:12:07 +0100

    fixing grid and popup

--- src/components/InstagramGrid/InstagramGrid.jsx
+++ src/components/InstagramGrid/InstagramGrid.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import InstagramCard from '../InstagramCard/InstagramCard'
-import InstagramPopup from '../InstagramPopup/InstagramPopup'
-import styles from './InstagramGrid.module.css'
-
+import React from "react"
+import InstagramCard from "../InstagramCard/InstagramCard"
+import InstagramPopup from "../InstagramPopup/InstagramPopup"
+import styles from "./InstagramGrid.module.css"
+import useBreakpoint from "../../shared/hooks/useBreakpoint"
 
 const Instagram = () => {
+  const bp = useBreakpoint()
+
   return (
     <div className={`${styles.instagram_wrapper}`}>
       <h3 className={`${styles.instagram_header}`}>Our instagram</h3>
-      <div className={`${styles.instagram_grid}`}>
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
-        <InstagramCard />
+      <div
+        className={`${styles.instagram_grid}`}
+        style={{
+          gridTemplateColumns: bp.lessThan("md")
+            ? `repeat(2, minmax(0, 1fr))`
+            : bp.lessThan("lg")
+            ? `repeat(4, minmax(0, 1fr))`
+            : `repeat(5, minmax(0, 1fr))`,
+        }}>
+        {[...Array(bp.lessThan("md") ? 6 : bp.lessThan("lg") ? 8 : 10)]?.map((card) => (
+          <InstagramCard />
+        ))}
 
         <div className={`${styles.instagram_popup_wrapper}`}>
           <InstagramPopup />
         </div>
-        
       </div>
-    
-    
     </div>
   )
 }
