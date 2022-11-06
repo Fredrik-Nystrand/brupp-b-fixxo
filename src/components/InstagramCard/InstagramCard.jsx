1c86a191a8cad2d20bb003f4e3764ef5fd5f3531
Magnus Ljung <ljung.magnus@gmail.com>
2022-11-06 15:31:37 +0100

    uppdaterat insta med senaste main

--- src/components/InstagramCard/InstagramCard.jsx
+++ src/components/InstagramCard/InstagramCard.jsx
@@ -6,7 +6,7 @@ const InstagramCard = () => {
   return (
     <div className='instagram-card'>
      <div className="instagram-card-img-container">
-        <img className='instagram-card-img' src={fakeimage} alt="bild" />
+        <img className='instagram-card-img' src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F8e%2Fbd%2F8ebdd2a3c55a4bafa2c250bfc46db2bb656180a5.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" alt="bild" />
       </div>
     </div>
   )
