4117b91fda8058ec7c03acba6afe736e1c036830
Fredrik Nystrand <f.nystrand@gmail.com>
2022-11-17 10:37:50 +0100

    final touches

--- src/Views/SingleProductPage/SingleProductPage.jsx
+++ src/Views/SingleProductPage/SingleProductPage.jsx
@@ -1,12 +1,19 @@
-import React from "react"
+import { useEffect } from "react"
 import DetailView from "../../components/SingleProduct/DetailView/DetailView"
 import InfoView from "../../components/SingleProduct/InfoView/InfoView"
+import { useParams } from "react-router-dom"
+import { useGetProductQuery } from "../../Store/api/apiSlice"
 
 const SingleProductPage = () => {
-  return <div className="container-inner">
-      <DetailView/>
-      <InfoView/>
-  </div>
+  const { id } = useParams()
+  const { data } = useGetProductQuery(id)
+
+  return (
+    <div className="container-inner">
+      <DetailView product={data} />
+      <InfoView />
+    </div>
+  )
 }
 
 export default SingleProductPage
